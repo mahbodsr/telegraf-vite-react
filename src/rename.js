f8ba4d68d2609ac7990caa3bd05488dae864c1b2
mahbodsr <srmahbod@gmail.com>
2024-08-20 21:10:42 +0330

    unlink reverted to rename

--- src/rename.js
+++ src/rename.js
@@ -4,4 +4,4 @@ const path = require("path");
 const OLD = path.join(__dirname, "..", "dist", "index.html");
 const NEW = path.join(__dirname, "..", "dist", "root.html");
 
-if (fs.existsSync(OLD)) fs.unlinkSync(OLD, NEW);
+if (fs.existsSync(OLD)) fs.rename(OLD, NEW);
